fb82c7bc4880fb5883cadf1b2a8caf614fe3eb8d
David Schilling <davejs92@gmail.com>
2014-07-21 13:31:12 +0200

    moved properties to properties file

--- src/main/java/com/ratemytree/rmt/config/MongoDBConfig.java
+++ src/main/java/com/ratemytree/rmt/config/MongoDBConfig.java
@@ -3,8 +3,13 @@ package com.ratemytree.rmt.config;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import java.net.UnknownHostException;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
@@ -15,14 +20,19 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
  */
 @Configuration
 @EnableMongoRepositories(basePackages = "com.ratemytree.rmt")
+@PropertySource(value = {"classpath:/application.properties", "classpath:/application-${environment:dev}.properties"})
 public class MongoDBConfig {
+
+    @Resource
+    private Environment environment;
+
     @Bean
     public
     MongoClient mongo() throws Exception {
-        return new MongoClient("localhost");
+        return new MongoClient(environment.getRequiredProperty("mongodb.hostname"));
     }
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongo(), "mydb");
+        return new MongoTemplate(mongo(), environment.getRequiredProperty("mongodb.db"));
     }
 }
