d14df7ac851196bb780cab04948031efb6fc9a18
David Schilling <davejs92@gmail.com>
2014-08-06 17:42:10 +0200

    add rest api to create user

--- src/main/java/com/ratemytree/rmt/user/AuthenticationController.java
+++ src/main/java/com/ratemytree/rmt/user/AuthenticationController.java
@@ -1,11 +1,13 @@
 package com.ratemytree.rmt.user;
 
 import java.security.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,12 +17,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AuthenticationController {
 
+    private final UserService userService;
+
+    @Autowired
+    public AuthenticationController(UserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public HttpEntity<User> login(Principal principal) {
 
         User userDTO = (User)((UsernamePasswordAuthenticationToken) principal).getPrincipal();
         return new ResponseEntity<>(userDTO, HttpStatus.OK);
+    }
 
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public HttpEntity createUser(@RequestBody User user) {
+        userService.createUser(user.getUsername(), user.getEmail(), user.getPassword());
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/ratemytree/rmt/user/User.java
+++ src/main/java/com/ratemytree/rmt/user/User.java
@@ -27,10 +27,13 @@ public class User implements UserDetails {
     @Field
     private String email;
 
-    public User(String username, String password, String email) {
+    public User(String username, String email, String password) {
         this.username = username;
         this.password = password;
         this.email = email;
+    }
+
+    public User() {
     }
 
     @Override
@@ -49,6 +52,22 @@ public class User implements UserDetails {
     @Override
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     @Override
